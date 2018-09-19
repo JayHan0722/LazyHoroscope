06464e1ed73a3ce2fa9834b91af7fc9c51173731
JayHan0722 <jhan2@mail.csuchico.edu>
2018-09-19 16:11:44 -0700

    Type in First and Last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Jaehyun Han
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,15 @@ using namespace std;
 int main()
 {
   //declare variables
+  string FN;
+  string LN;
 
   //get user input
+  cout<<"Type in your First name: "<<endl;
+  cin>>FN;
+
+  cout<<"Type in your Last name: "<<endl;
+  cin>>LN;
 
   //tell fortune
 
