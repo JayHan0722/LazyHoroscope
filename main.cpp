f4567e2cc62a195c69c143f31dade44324a2d884
JayHan0722 <jhan2@mail.csuchico.edu>
2018-09-24 16:34:29 -0700

    Fortune Telling Program

--- main.cpp
+++ main.cpp
@@ -11,15 +11,37 @@ int main()
   string Last;
 
   //get user input
-  cout<<"Type in your First name: "<<endl;
+  cout<<"What is your First name?"<<endl;
   cin>>First;
 
-  cout<<"Type in your Last name: "<<endl;
+  cout<<"What is your Last Name?"<<endl;
   cin>>Last;
 
   //tell fortune
   cout<<"Welcome, "<<First[0]<<"."<<Last[0]<<"."<<", here is your fourtune..."<<endl;
   cout<<"Your lucky number is "<<First.length()<<endl;
+
+  if(First[0] == 'a' || First[0] == 'e' || First[0] == 'i' || First[0] == 'o' || First[0] == 'u' ||
+     First[0] == 'A' || First[0] == 'E' || First[0] == 'I' || First[0] == 'O' || First[0] == 'U')
+  {
+          cout<<"You are destined to be famous."<<endl;
+  }
+
+  else
+  {
+          cout<<"You should keep a low profile."<<endl;
+  }
+  
+  int index = Last.length() - 1;
+  char LL = Last[index];
+
+  if(LL == 'a' || LL == 'e' || LL == 'i' || LL == 'o' || LL == 'u' ||
+     LL == 'A' || LL == 'E' || LL == 'I' || LL == 'O' || LL == 'U')
+  {
+          cout<<"You have already met your ture love."<<endl;
+  }
+
+  cout<<"Have a good day!"<<endl;
 
 
   return 0;
